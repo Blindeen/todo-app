afa2abed8bd39532ef446ae0a629d11f44c401ac
Blindeen <kuba.szmajda@gmail.com>
2023-07-14 16:42:35 +0200

    add validating function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 export const setLocalStorage = (key: string, value: string) => localStorage.setItem(key, value);
 
 export const getLocalStorage = (key: string) => localStorage.getItem(key) || "";
+
+export const isStringValid = (str: string) => {
+  const regex = /^[^A-Z0-9a-z]/;
+
+  return !regex.test(str);
+}
