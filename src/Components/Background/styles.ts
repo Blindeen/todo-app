f808f25d67be3cf3b569bca4d4d5aa4e524020e3
Blindeen <kuba.szmajda@gmail.com>
2023-09-18 18:36:54 +0200

    create common components

--- src/Components/Background/styles.ts
+++ src/Components/Background/styles.ts
@@ -12,10 +12,14 @@ export const BackgroundContainer = styled("div")`
 `;
 
 export const NotebookContainer = styled("div")`
-  height: 500px;
-  width: 450px;
+  height: 450px;
+  width: 400px;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
   
   background-color: ${palette.freshwaterLemon};
+  font-family: Comic Sans MS, Comic Sans, cursive;
 `;
