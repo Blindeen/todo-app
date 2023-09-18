c92d0a8d17b1f63b493a6fcbb591c8801cae8008
Blindeen <kuba.szmajda@gmail.com>
2023-09-18 19:41:04 +0200

    create loader component

---
+++ src/Components/Loader/index.tsx
@@ -0,0 +1,11 @@
+import {StyledBackground, StyledLoader} from "./styles";
+
+const Loader = () => {
+  return (
+    <StyledBackground>
+      <StyledLoader/>
+    </StyledBackground>
+  );
+}
+
+export default Loader;
