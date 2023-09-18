d17a74d72d852e8034a20aeb52497a86bfb3d80f
Blindeen <kuba.szmajda@gmail.com>
2023-09-18 18:41:51 +0200

    change routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {Routes, Route} from "react-router-dom"
 
 import TODOPage from "./Pages/TODO page";
+import LoginPage from "./Pages/Login page";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<TODOPage/>}/>
+        <Route path="todo-app" element={<TODOPage/>}/>
+        <Route path="login" element={<LoginPage/>}/>
       </Routes>
     </div>
   )
