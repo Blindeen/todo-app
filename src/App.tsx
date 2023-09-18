06b9eda13e65d46e33e1ca923502c7f5ce072a38
Blindeen <kuba.szmajda@gmail.com>
2023-09-18 13:22:36 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import {Routes, Route} from "react-router-dom"
+
 import TODOPage from "./Pages/TODO page";
 
 function App() {
-  return <TODOPage />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<TODOPage/>}/>
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
