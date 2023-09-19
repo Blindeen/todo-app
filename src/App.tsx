cf56547365789a39ce6ece1a9244adedb06ac6dd
Blindeen <kuba.szmajda@gmail.com>
2023-09-19 12:29:38 +0200

    move routes to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import TODOPage from './Pages/TODO page';
 import LoginPage from './Pages/Login page';
 
+import { routes } from './routes';
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='todo-app' element={<TODOPage />} />
-        <Route path='login' element={<LoginPage />} />
+        <Route path={routes.todo} element={<TODOPage />} />
+        <Route path={routes.login} element={<LoginPage />} />
       </Routes>
     </div>
   );
