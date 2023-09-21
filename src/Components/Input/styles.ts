7c97a55856f28882d4d0f605d3ac9920739f79a6
Blindeen <kuba.szmajda@gmail.com>
2023-09-21 17:54:40 +0200

    refactor input component

--- src/Components/Input/styles.ts
+++ src/Components/Input/styles.ts
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 
+import palette from 'palette';
+
 export const StyledInput = styled('input')`
   width: 100%;
   height: 30px;
+
+  border: none;
+  border-bottom: 2px solid;
+  background: ${palette.freshwaterLemon} !important;
 
   font-family:
     Comic Sans MS,
     Comic Sans,
     cursive;
+
+  &:focus {
+    outline: none;
+  }
 `;
